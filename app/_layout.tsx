38d614ced3e3887831782ce4f00e6045b81b0d75
FabLrc <fabien.laurence33@gmail.com>
2025-10-27 12:32:17 +0100

    Ajout de l'icône Home dans le header de navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,34 @@
-import { Stack } from "expo-router";
+import { Feather } from "@expo/vector-icons";
+import { Stack, useRouter } from "expo-router";
+import { StyleSheet } from "react-native";
 
 export default function RootLayout() {
+  const router = useRouter();
   return (
     <Stack>
-      <Stack.Screen name="page1"/>
-      <Stack.Screen name="(page2)"/>
+      <Stack.Screen name="page1" />
+      <Stack.Screen
+        name="(page2)"
+        options={{
+          headerTitle: "",
+          headerLeft: () => (
+            <Feather
+              style={styles.iconStyle}
+              name="home"
+              size={24}
+              color="black"
+              onPress={() => router.back()}
+            />
+          ),
+        }}
+      />
     </Stack>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  iconStyle: {
+    marginRight: 0,
+    paddingLeft: 10,
+  },
+});
