7b43f6a60f467dc9b01fe044c9b444c3d548837f
FabLrc <fabien.laurence33@gmail.com>
2025-10-27 11:58:18 +0100

    Mettre les boutons à l'horizontal

--- app/index.tsx
+++ app/index.tsx
@@ -1,24 +1,25 @@
-import { Link, useRouter } from "expo-router";
-import { Text, View, Button } from "react-native";
+import { useRouter } from "expo-router";
+import { Button, StyleSheet, View } from "react-native";
 
 export default function Index() {
   const router = useRouter();
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Button onPress={()=>router.push("/page1")} 
-      title="Page 1"/>
-      <Button onPress={()=>router.push("/tab1")} 
-      title="Page 2"/>
+    <View style={styles.viewStyle}>
+      <Button onPress={() => router.push("/page1")} title="Page 1" />
+      <Button onPress={() => router.push("/tab1")} title="Page 2" />
 
-     {/* <Link href="/page1">PAGE1</Link>
+      {/* <Link href="/page1">PAGE1</Link>
       <Link href="/tab1">PAGE2</Link>*/}
-      
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  viewStyle: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 16,
+  },
+});
